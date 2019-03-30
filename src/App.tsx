9e9570718351cf52ae58f8d16ead9fc9ae8b8b93
takaaa220 <taaaaaaaaaa220@gmail.com>
2019-03-30 20:27:53 +0900

    add settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,16 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 
-export interface AppProps {
-}
+export interface AppProps {}
 
-export interface AppState {
-}
+export interface AppState {}
 
 class App extends React.Component<AppProps, AppState> {
   render() {
-    return (
-      <div>
-        hello
-      </div>
-    );
+    return <div />;
   }
 }
 
 export default App;
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
