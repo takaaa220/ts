eb61536a0b73f87f28a260a6cccb906605ef25e1
takaaa220 <taaaaaaaaaa220@gmail.com>
2019-03-30 16:52:55 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+export interface AppProps {
+}
+
+export interface AppState {
+}
+
+class App extends React.Component<AppProps, AppState> {
+  render() {
+    return (
+      <div>
+        hello
+      </div>
+    );
+  }
+}
+
+export default App;
+
+ReactDOM.render(<App />, document.getElementById('root'));
